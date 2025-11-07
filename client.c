13b90f0556482afb37a51de429997bf7bf628cc3
squassa <gabriel.squassoni@gmail.com>
2025-11-07 15:22:05 -0300

    Updated client

--- client.c
+++ client.c
@@ -3,56 +3,119 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include "utils.h"
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+#define PORTA 3124
+#define WIDTH 2000
+#define HEIGHT 2000
+
+// Função de suavização com stencil de 5 pontos
+void smoothImage(int linhas, int colunas, int imagem[linhas][colunas], int resultado[linhas][colunas]) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            int soma = imagem[i][j];
+            int cont = 1;
+
+            if (i > 0) { soma += imagem[i - 1][j]; cont++; }
+            if (i < linhas - 1) { soma += imagem[i + 1][j]; cont++; }
+            if (j > 0) { soma += imagem[i][j - 1]; cont++; }
+            if (j < colunas - 1) { soma += imagem[i][j + 1]; cont++; }
+
+            resultado[i][j] = soma / cont;
+        }
+    }
 }
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    static int image[HEIGHT][WIDTH];
-    static int result[HEIGHT][WIDTH];
+    int sockfd;
+    struct sockaddr_in servidor_addr;
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        error("Erro ao criar socket");
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-        error("Endereço inválido");
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("Falha na conexão");
-
-    printf("Conectado ao servidor.\n");
-
-    // Recebe a imagem
-    ssize_t received = recv(sock, image, sizeof(image), MSG_WAITALL);
-    if (received != sizeof(image))
-        fprintf(stderr, "Aviso: recebi %ld bytes, esperava %ld\n", received, sizeof(image));
-
-    printf("Imagem recebida (%ld bytes)\n", received);
-
-    // Processa a imagem
-    for(int i = 0; i < HEIGHT; i++)
-    {
-        for(int j = 0; j < WIDTH; j++)
-        {
-            printf("%d ", image[i][j]);
-        }
-        printf("\n");
+    // Criação do socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Erro ao criar socket");
+        exit(1);
     }
 
-    smooth_image(image, result);
+    servidor_addr.sin_family = AF_INET;
+    servidor_addr.sin_port = htons(PORTA);
+    servidor_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP do servidor
 
-    // Envia o resultado
-    send(sock, result, sizeof(result), 0);
-    printf("Resultado enviado.\n");
+    // Conexão ao servidor
+    if (connect(sockfd, (struct sockaddr *)&servidor_addr, sizeof(servidor_addr)) < 0) {
+        perror("Erro ao conectar ao servidor");
+        close(sockfd);
+        exit(1);
+    }
 
-    close(sock);
+    printf("[CLIENTE] Conectado ao servidor.\n");
+
+    // Recebe número de blocos que este cliente vai processar
+    int num_blocos;
+    recv(sockfd, &num_blocos, sizeof(int), 0);
+    printf("[CLIENTE] Receberá %d bloco(s).\n", num_blocos);
+
+    for (int b = 0; b < num_blocos; b++) {
+        int linhas, colunas;
+        recv(sockfd, &linhas, sizeof(int), 0);
+        recv(sockfd, &colunas, sizeof(int), 0);
+
+        printf("[CLIENTE] Recebendo bloco %d (%dx%d)...\n", b + 1, linhas, colunas);
+
+        // Aloca memória dinamicamente para o bloco
+        int (*image)[colunas] = malloc(linhas * sizeof(*image));
+        int (*result)[colunas] = malloc(linhas * sizeof(*result));
+
+        if (!image || !result) {
+            perror("Erro de alocação de memória");
+            close(sockfd);
+            exit(1);
+        }
+
+        // Recebe pixels do bloco
+        int bytes_esperados = linhas * colunas * sizeof(int);
+        int recebidos = 0;
+        char *ptr = (char *)image;
+
+        while (recebidos < bytes_esperados) {
+            int n = recv(sockfd, ptr + recebidos, bytes_esperados - recebidos, 0);
+            if (n <= 0) {
+                perror("Erro ao receber bloco de imagem");
+                free(image);
+                free(result);
+                close(sockfd);
+                exit(1);
+            }
+            recebidos += n;
+        }
+
+        printf("[CLIENTE] Bloco %d recebido. Processando...\n", b + 1);
+        smoothImage(linhas, colunas, image, result);
+
+        // Envia resultado de volta ao servidor
+        int enviados = 0;
+        char *ptr_envio = (char *)result;
+        int total_bytes = linhas * colunas * sizeof(int);
+
+        while (enviados < total_bytes) {
+            int n = send(sockfd, ptr_envio + enviados, total_bytes - enviados, 0);
+            if (n <= 0) {
+                perror("Erro ao enviar resultado");
+                free(image);
+                free(result);
+                close(sockfd);
+                exit(1);
+            }
+            enviados += n;
+        }
+
+        printf("[CLIENTE] Bloco %d enviado com sucesso!\n", b + 1);
+
+        free(image);
+        free(result);
+    }
+
+    printf("[CLIENTE] Todos os blocos processados e enviados.\n");
+
+    close(sockfd);
     return 0;
 }
